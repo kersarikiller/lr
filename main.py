96b54e74d54e96c882a586a9f08399744302d5d4
liza <millipede21@gmail.com>
2024-11-18 02:54:23 +0300

    adding changes in main file from person3

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from quote_manager import load_quotes, random_quote, add_quote
+from datetime import datetime
 
 def main():
     q = load_quotes()
-    print(f'random quote: {random_quote(q)}', f'total quotes: {len(q)}')
+    date_added = datetime.now().strftime("%Y-%m-%d")
+    print(f'random quote: {random_quote(q)}', f'total quotes: {len(q)}', date_added)
     a = input('add a quote? print "y" or "n" \n')
     if a == 'y':
         add_quote(input('add a quote: \n'))
