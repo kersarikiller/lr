607f17637c5028482c86f1b8a163000a60fdc0ac
liza <millipede21@gmail.com>
2024-11-17 17:21:37 +0300

    adding changes to file main.py from person1

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from quote_manager import load_quotes, random_quote
+from quote_manager import load_quotes, random_quote, add_quote
 
 def main():
     q = load_quotes()
-    print(f'random quote: {random_quote(q)}')
+    print(f'random quote: {random_quote(q)}', f'total quotes: {len(q)}')
+    a = input('add a quote? print "y" or "n" \n')
+    if a == 'y':
+        add_quote(input('add a quote: \n'))
+
 
 if __name__ == '__main__':
     main()
