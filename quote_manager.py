9d4e88e429b8a6057b0eb1315f013281c74e4f32
liza <millipede21@gmail.com>
2024-11-18 03:01:35 +0300

    adding changes in quote_manager file from person2

--- quote_manager.py
+++ quote_manager.py
@@ -10,3 +10,9 @@ def random_quote(quotes):
 def add_quote(quote):
     with open('quotes.txt', 'a', encoding='utf-8') as f:
             f.write(f'{quote}\n')
+
+def list_quotes_by_length(descending=False):
+    quotes = load_quotes()
+    sorted_quotes = sorted(quotes, key=len, reverse=descending)
+    for quote in sorted_quotes:
+        print(quote)
